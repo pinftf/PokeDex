f527126103634d7432d697b549df700ae7413be9
Joao Pinto <pinftf@gmail.com>
2023-10-25 06:30:36 +0100

    list functionality done

--- js/main.js
+++ js/main.js
@@ -9,25 +9,36 @@ for (let i = 1; i <= 151; i++) {
 }
 
 let showPokemon = function (poke) {
+  let types = poke.types.map(
+    (type) => `<p class="${type.type.name} type">${type.type.name}</p>`
+  );
+  types = types.join(" ");
+
+  let pokeId = poke.id.toString();
+  if (pokeId.length === 1) {
+    pokeId = "00" + pokeId;
+  } else if (pokeId.length === 2) {
+    pokeId = "0" + pokeId;
+  }
+
   const div = document.createElement("div");
   div.classList.add("pokemon");
-  div.innerHTML = ` <p class="pokemon-id-back">#025</p>
+  div.innerHTML = ` <p class="pokemon-id-back">${poke.id}</p>
   <div class="pokemon-image">
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-          alt="Pikachu">
+      <img src="${poke.sprites.other["official-artwork"].front_default}"
+          alt="${poke.name}">
   </div>
   <div class="pokemon-info">
       <div class="name-container">
-          <p class="pokemon-id">#025</p>
+          <p class="pokemon-id">${poke.id}</p>
           <h2 class="pokemon-name">${poke.name}</h2>
       </div>
       <div class="pokemon-types">
-          <p class="eletric type">ELECTRIC</p>
-          <p class="fighting type">FIGHTING</p>
+          ${types}
       </div>
       <div class="pokemon-stats">
-          <p class="stat">4m</p>
-          <p class="stat">60kgs</p>
+          <p class="stat">${poke.height}</p>
+          <p class="stat">${poke.weight}</p>
       </div>
   `;
 
